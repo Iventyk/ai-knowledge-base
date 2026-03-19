6f059be9f07fce67753e46c39714c292ce2f2509
ivan <ivanventik@gmail.com>
2026-03-19 14:23:00 +0200

    feat: added configs

---
+++ src/core/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    database_url: str
+    redis_url: str
+    openai_api_key: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
