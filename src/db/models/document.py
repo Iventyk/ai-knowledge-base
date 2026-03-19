1f6622ba9327c1547878b32c211f653eeaf74d87
ivan <ivanventik@gmail.com>
2026-03-19 14:25:13 +0200

    feat: added document models

---
+++ src/db/models/document.py
@@ -0,0 +1,18 @@
+import uuid
+from datetime import datetime
+
+from sqlalchemy import String, Integer, DateTime, func
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.db.base import Base
+
+
+class Document(Base):
+    __tablename__ = "documents"
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name: Mapped[str] = mapped_column(String)
+    status: Mapped[str] = mapped_column(String, default="processing")
+    chunks_count: Mapped[int] = mapped_column(Integer, default=0)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False,)
