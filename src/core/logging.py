2816dae6b152d6355a5ea69b8e1d034d5c6b6d79
ivan <ivanventik@gmail.com>
2026-03-21 16:02:09 +0200

    feat: changed logging

--- src/core/logging.py
+++ src/core/logging.py
@@ -1,15 +1,30 @@
 import logging
 import sys
 
+import structlog
 
-def setup_logging():
-    """
-    Setup global logging configuration for the project.
-    Logs will be output to stdout.
-    """
-    log_format = "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+from src.core.config import settings
+
+
+def setup_logging() -> None:
+    timestamper = structlog.processors.TimeStamper(fmt="iso")
+    shared_processors = [
+        structlog.stdlib.add_log_level,
+        timestamper,
+        structlog.processors.StackInfoRenderer(),
+        structlog.processors.format_exc_info,
+    ]
+
     logging.basicConfig(
-        level=logging.INFO,
-        format=log_format,
-        handlers=[logging.StreamHandler(sys.stdout)],
+        format="%(message)s",
+        stream=sys.stdout,
+        level=getattr(logging, settings.log_level.upper(), logging.INFO),
+    )
+
+    structlog.configure(
+        processors=shared_processors
+        + [structlog.processors.JSONRenderer()],
+        wrapper_class=structlog.stdlib.BoundLogger,
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        cache_logger_on_first_use=True,
     )
